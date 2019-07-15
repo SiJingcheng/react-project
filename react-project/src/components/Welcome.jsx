2e785f23c5521fc20ba2c1417f3e96d5d91ffef4
sjc <971803516@qq.com>
2019-07-15 23:09:31 +0800

    add Welcome.jsx

---
+++ react-project/src/components/Welcome.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Welcome extends Component {
+    state = {  }
+    handleClick=(e)=>{
+        return <Redirect to="/SignIn"/>
+    }
+    render() { 
+        return ( 
+            <div className="container">
+                <h1>Welcome</h1>
+                <button onclick={this.handleClick}>SignOut</button>
+            </div>
+    );
+    }
+}
+ 
+export default Welcome;
